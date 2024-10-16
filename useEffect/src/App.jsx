bde7dd92cdfb119c0f478b4f78c6a978c259e670
ajojose803 <ajojose803@gmail.com>
2024-10-16 21:20:55 +0530

    useEffect

---
+++ useEffect/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+import ProfileCard from './profileCard';
+
+function App() {
+
+
+  return (
+
+    <div className="App">
+      <ProfileCard />
+    </div>
+  );
+}
+export default App
