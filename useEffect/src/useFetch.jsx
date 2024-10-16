b9ffca0563890b252c2e4c346e180cd1d8bb973e
ajojose803 <ajojose803@gmail.com>
2024-10-16 21:46:05 +0530

    Custom Hook (useFetch) created

--- useEffect/src/useFetch.jsx
+++ useEffect/src/useFetch.jsx
@@ -1,1 +1,35 @@
 import { useState, useEffect } from "react";
+
+const useFetch = (url) =>{
+    const [data,setData] = useState(null);
+    const [error,setError] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    
+    useEffect(() => {
+        const abortCont = new AbortController();
+
+        fetch(url, {signal:abortCont.signal})
+        .then(res =>{
+            if(!res.okay) {
+                throw Error ("could not fetch data")
+            }
+            return res.json();
+        })
+        .then(data => {
+            setData(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            if(err.name == 'AbortError') {
+                console.log("Fetch Aborted")
+            } else {
+                setIsPending(false);
+                setError(err.message);
+            }
+        })
+        return () => abortCont.abort();
+    },[url])
+    return {data, isPending, error};
+}
+export default useFetch
