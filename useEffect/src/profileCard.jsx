02997c259cf8c83781745577407a48b235c38faa
ajojose803 <ajojose803@gmail.com>
2024-10-17 11:34:07 +0530

    updated tailwind css

--- useEffect/src/profileCard.jsx
+++ useEffect/src/profileCard.jsx
@@ -16,25 +16,29 @@ export default function ProfileCard() {
         }
     }
 
-    function handleDecrement(){
-        if(id > 1) {
-            setId( id - 1)
+    function handleDecrement() {
+        if (id > 1) {
+            setId(id - 1)
         } else {
             setId(1);
         }
     }
     return (
-        <div className="profileCardContainer">
+        <div className="bg-gray-100 flex flex-row justify-center">
             {isPending && <div> loading...</div>}
             {error && <div> {error} </div>}
             {user && (
-                <div className="profile_card" key={user.id}>
-                    <h1> {id}</h1>
-                    <h1>{user.name}</h1>
-                    <p>Email: {user.email}</p>
-                    <div className="buttonContainer">
-                        <button onClick={handleIncrement}> +</button>
-                        <button onClick={handleDecrement}>-</button>
+                <div className="" key={user.id}>
+                    <h1 className=' mt-5 font-sans text-2xl font-bold flex justify-center'> {id}</h1>
+                    <div className=' py-10 mt-5 bg-emerald-600 '>
+                        <h1 className='text-slate-100 font-sans text-4xl  font-bold flex justify-center'>{user.name}</h1>
+                        <p className='font-mono text-base flex justify-center' >Email: {user.email}</p>
+                    </div>
+                    <div className=" mt-5 flex flex-col">
+                        <button className=" mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-screen"
+                            onClick={handleIncrement}> +</button>
+                        <button className=" mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-screen"
+                            onClick={handleDecrement}>-</button>
                     </div>
                 </div>
             )}
